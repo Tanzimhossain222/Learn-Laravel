e19a640bd02e687af6deec6d022b47595e9cda7e
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-07-30 14:40:19 +0600

    feat: create a connection between employer and jobs

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,35 +1,23 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            ['id' => 1, 'title' => 'Director', 'salary' => '$50,000'],
-            ['id' => 2, 'title' => 'Programmer', 'salary' => '$150,000'],
-            ['id' => 3, 'title' => 'Teacher', 'salary' => '$30,000'],
-            ['id' => 4, 'title' => 'Designer', 'salary' => '$70,000'],
-            ['id' => 5, 'title' => 'Engineer', 'salary' => '$100,000'],
-            ['id' => 6, 'title' => 'Doctor', 'salary' => '$200,000'],
-            ['id' => 7, 'title' => 'Nurse', 'salary' => '$40,000'],
-            ['id' => 8, 'title' => 'Accountant', 'salary' => '$60,000'],
-            ['id' => 9, 'title' => 'Manager', 'salary' => '$80,000'],
-            ['id' => 10, 'title' => 'Secretary', 'salary' => '$20,000'],
-        ];
+    use HasFactory;
+    
+    protected $table = 'job_listings';
+
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
     }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job["id"] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    
 };
