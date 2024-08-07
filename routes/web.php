4357cf50d32241687134a2cef2a982b4764a5c2f
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-08-07 08:23:26 +0600

    25, 26: Send mail & Queues

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterUserController;
 use App\Http\Controllers\SessionController;
+use App\Mail\JobPosted;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+
 
 
 Route::view('/', 'home');
@@ -14,6 +17,7 @@ Route::view('contact', 'contact');
 // jobs routes
 Route::controller(JobController::class)->group(function () {
     Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
     Route::get('/jobs/{job}',  'show');
     Route::post('/jobs',  'store')->middleware('auth');
     Route::get('/jobs/{job}/edit',  'edit')->middleware('auth')->can('edit', 'job');
